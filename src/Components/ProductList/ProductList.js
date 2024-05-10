10017f742d76e9a75a688380bac6f500e112add9
Harshada <hyadav8701@conestogac.on.ca>
2024-05-09 22:41:27 -0400

    added sorting of products

--- src/Components/ProductList/ProductList.js
+++ src/Components/ProductList/ProductList.js
@@ -1,12 +1,12 @@
-// Importing components
-import React, { useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
 // Importing UI components
 import Product from "../UI/Product";
 
 // Importing from redux
-
 import {
   fetchProductsAsync,
   selectProducts,
@@ -15,8 +15,18 @@ import {
 
 const ProductList = () => {
   const dispatch = useDispatch();
-  const products = useSelector(selectProducts);
+  const categories = useSelector(selectProducts);
   const status = useSelector(selectProductsStatus);
+  const [sortBy, setSortBy] = useState(""); // State to store sorting option
+
+  const sortOptions = [
+    { name: "Name", value: "name" },
+    { name: "Price", value: "price" },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
 
   useEffect(() => {
     if (status === "idle") {
@@ -24,19 +34,77 @@ const ProductList = () => {
     }
   }, [status, dispatch]);
 
+  const handleSortChange = (value) => {
+    setSortBy(value);
+  };
+
+  const sortedProducts = categories.reduce((accumulator, category) => {
+    return accumulator.concat(category.products);
+  }, []);
+
+  if (sortBy === "price") {
+    sortedProducts.sort((a, b) => a.price - b.price);
+  } else if (sortBy === "name") {
+    sortedProducts.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
   return (
     <div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-          All Products
-        </h2>
+        <div className="flex items-baseline justify-between ">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+            All Products
+          </h2>
+          <Menu as="div" className="relative inline-block text-left">
+            <div>
+              <Menu.Button className="group inline-flex justify-center text-md font-medium text-gray-700 hover:text-gray-900">
+                Sort
+                <ChevronDownIcon
+                  className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <div className="py-1">
+                  {sortOptions.map((option) => (
+                    <Menu.Item key={option.name}>
+                      {({ active }) => (
+                        <button
+                          onClick={() => handleSortChange(option.value)}
+                          className={classNames(
+                            sortBy === option.value
+                              ? "font-medium text-gray-900"
+                              : "text-gray-500",
+                            active ? "bg-gray-100" : "",
+                            "block px-4 py-2 text-sm"
+                          )}
+                        >
+                          {option.name}
+                        </button>
+                      )}
+                    </Menu.Item>
+                  ))}
+                </div>
+              </Menu.Items>
+            </Transition>
+          </Menu>
+        </div>
 
         <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8">
-          {products.map((category) =>
-            category.products.map((product) => (
-              <Product key={product.id} product={product} />
-            ))
-          )}
+          {sortedProducts.map((product) => (
+            <Product key={product._id} product={product} />
+          ))}
         </div>
       </div>
     </div>
